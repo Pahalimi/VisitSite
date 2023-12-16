4fc2c5fa89142d39d3990b2b2e8b5c5981c293d9
Pavel Kos <pk6937074@gmail.com>
2023-12-17 01:06:50 +0300

    first-commit

---
+++ main/dop/script.js
@@ -0,0 +1,12 @@
+const hide = document.getElementById("hide");
+const hide2 = document.getElementById("hide2");
+const hide3 = document.getElementById("hide3");
+
+function showme() {
+    hide.style.display = "block";
+    hide2.style.display = "block";
+}
+
+function showme2() {
+    hide3.style.display = "block";
+}
---
+++ main/script.js
@@ -0,0 +1,11 @@
+const button = document.getElementById("btn_c");
+
+function myFunc() {
+    location.href = "#openModal";
+}
+
+const gipper = document.getElementsByClassName("btn_table");
+
+function gipperFunc() {
+    location.href = "dop/dop.html";
+}
